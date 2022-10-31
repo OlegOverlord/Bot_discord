887930913ebd3ccace45ed85e4f8b48ac8af29b8
Liguha <artysmi@mail.ru>
2022-10-31 23:16:40 +0300

    Alpha

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const { joinVoiceChannel, VoiceConnectionStatus } = require('@discordjs/voice');
 const { createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 const { createAudioPlayer, NoSubscriberBehavior } = require('@discordjs/voice');
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 const config = require("./config.json");
 
 const client = new Client(
